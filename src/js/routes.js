78dbeb31767ca9fb72fa2d460b365afe5eccc114
ANTON ORTEGA RIVERA <antonortega17@gmail.com>
2020-07-20 11:57:23 -0500

    mail
    sending mail

--- src/js/routes.js
+++ src/js/routes.js
@@ -34,14 +34,11 @@ var routes = [
 
     app.request.json(stripeUrl+'/'+sessionId, function (res) {
         app.preloader.hide();
-
         console.log(res);
-
         console.log(res.payment_intent);
+        var paymentIntent = res.payment_intent;
 
-        var payment_intent = res.payment_intent;
-
-        app.request.get(paymentUrl+'/'+payment_intent, function(paymentRes) {
+        app.request.get(paymentUrl+'/'+paymentIntent, function(paymentRes) {
           var paymentData = JSON.parse(paymentRes);
           
           if(paymentData.charges.data[0].paid)
@@ -51,7 +48,7 @@ var routes = [
             //--- Aqui ya tiene la info necesaria para dar de alta la informacion en la base de datos
             var paymentData = {
               status: 'confirmed',
-              gateway_id: payment_intent
+              payment_intent: paymentIntent
             };
 
             app.request.post(app.data.api+'/items/payments', paymentData, function(dbPaymentResponse) {
@@ -60,8 +57,16 @@ var routes = [
 
               var payment_newId = dbPaymentResponse.data.id;
               //--- TEMPORALMENTE UTILZIARE EL TIME AL ESTILO UNIX PARA GENERAL UN SERIAL UNICO
+
+              var serial_license;
+              app.methods.serialGen().then(function(res){
+                console.log(res);
+                serial_license = res;
+              });
+
+             /* 
               var serial_license = Math.floor(new Date() / 1000);
-              console.log('Serial_Licence: ' + serial_license);
+              console.log('Serial_Licence: ' + serial_license);*/
 
               var licenseData = {
                 active: 1,
@@ -84,6 +89,26 @@ var routes = [
                 app.request.post(app.data.api+'/items/clients', clientData, function(dbClientResponse){
                   dbClientResponse = JSON.parse(dbClientResponse);
                   console.log(dbClientResponse);
+
+                  //--- ya se han generado todos los datos en la base de datos, ahora debe enviar el correo al usuario, y avisar en la pagina que ya esta todo OK
+                  var email_data = {
+                    "to": [
+                      payment_email
+                    ],
+                    "subject": "Carl & the Plague",
+                    "body": "Gracias por adquirir el juego de <b>Carl & the Plague</b><br><br>Para instalar su copia del juego, descarguelo en esta direccion <a href:'{{direccion}}'>{{direccion}}</a><br><br>su numero de serie es<br><br><b>{{serial}}</b>",
+                    "type": "html",
+                    "data": {
+                      "serial": serial_license,
+                      "direccion": "http://www.wetheforce.com/juego_descarga"
+                    }
+                  }
+
+                  app.request.post(app.data.api+'/mail'. email_data, function(mail_response){
+                    //--- aqui mostrar la cosa de que ya se envio el correo y asi
+                    console.log("mail sent");
+                  });
+
                 });
               });
             });
